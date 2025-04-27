82f747437156adc8cf64ce09918f0efd18296a10
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2025-04-27 19:34:22 +0100

    logo added to nav

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,19 +2,23 @@ import Hamburger from "hamburger-react";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import BuyNowButton from "../components/BuyNowButton";
+import WHLogo from "../assets/WHLogo.png";
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <nav className="fixed backdrop-blur-lg p-4 w-full bg-background/90 shadow-md">
-      <div className="flex items-center justify-between">
-        {/* Left: Logo */}
-        <div className="text-2xl font-bold">
-          <Link to="/" className="text-black">
-            Brand
-          </Link>
-        </div>
+    <nav className="fixed backdrop-blur-lg w-full bg-background/90 shadow-md h-20 flex items-center">
+  <div className="flex items-center justify-between w-full px-4">
+    
+    {/* Left: Logo */}
+    <Link to="/" className="flex items-center">
+      <img
+        src={WHLogo}
+        alt="Wendy Hudson Logo"
+        className="max-h-20 w-auto" // <= 👈 Here
+      />
+    </Link>
 
         {/* Center: Nav Links (Desktop) */}
         <div className="hidden lg:flex space-x-6">
