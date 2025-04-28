f6cdd67ddd9e169f3a9e64fb0657cd576262972f
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2025-04-28 01:07:13 +0100

    books section text added

--- src/components/BooksSection.jsx
+++ src/components/BooksSection.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import SectionWrapper from "./SectionWrapper";
 
 const BooksSection = () => {
   return (
-    <div>BooksSection</div>
-  )
-}
+    <SectionWrapper noTopPadding>
+      <div className="w-full mx-auto flex flex-col items-center text-center">
+        <h2>Books</h2>
+        <p className="text-base">Browse Wendy’s published works and find your next favorite read.</p>
+      </div>
+      <div>
+        
+      </div>
+    </SectionWrapper>
+  );
+};
 
-export default BooksSection
+export default BooksSection;
